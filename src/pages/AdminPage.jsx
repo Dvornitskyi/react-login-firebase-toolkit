40634a28835d628be24a0b76db2626604b8af41a
Yan Dvornitskyi <dvornitskyi@gmail.com>
2021-12-10 09:55:48 +0200

    first commit

---
+++ src/pages/AdminPage.jsx
@@ -0,0 +1,21 @@
+import {Redirect} from 'react-router-dom';
+import {useDispatch} from 'react-redux'
+import { useAuth } from 'hooks/use-auth';
+import {removeUser} from 'store/slices/userSlice'
+
+const AdminPage = () => {
+    const dispatch = useDispatch();
+    const {isAuth, email} = useAuth();
+    return isAuth ? (
+        <div>
+            <h1>Welcome Admin</h1>
+            <button
+                onClick={()=> dispatch(removeUser())}
+            >Log out from {email}</button>
+        </div>
+    ) : (
+        <Redirect to="/login" />
+    )
+}
+
+export default AdminPage
